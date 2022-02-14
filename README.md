23fee37a3b0c497548c12111ba603ba1bb778a26
moin21 <mamoinuddin@gmail.com>
2022-02-14 23:09:43 +0530

    Added: Welcome message

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a program to compare length of two lines.
---
+++ src/com/linecomparisonprogram/LineComparisonProgram.java
@@ -0,0 +1,7 @@
+package com.linecomparisonprogram;
+
+public class LineComparisonProgram {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Program!!");
+	}
+}
