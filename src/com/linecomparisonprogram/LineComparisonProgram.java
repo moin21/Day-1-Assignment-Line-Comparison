02e1e67b80cf37b7c6b52c1c16bd9883efbcd7a1
Moinuddin Asraf <mamoinuddin@gmail.com>
2022-02-15 00:50:15 +0530

    Added: Logic for length calculation

--- src/com/linecomparisonprogram/LineComparisonProgram.java
+++ src/com/linecomparisonprogram/LineComparisonProgram.java
@@ -4,4 +4,8 @@ public class LineComparisonProgram {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Program!!");
 	}
+public static double lineLength(int i, int j, int k, int l) {
+		length = Math.sqrt((j - i) * (j - i) + (l - k) * (l - k));
+		return length;
+	}
 }
